33b0ebc5c3009dbfcb3652035491c483af241937
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-06-21 21:49:45 -0400

    all

---
+++ keep.js
@@ -0,0 +1,45 @@
+//===========================================
+
+function getEmail(user){
+
+  
+}
+
+//===========================================
+
+function getPlaylistLength(name, song){
+
+let song = [];
+
+for (let i =0; i < song.length; i++) {
+    
+}
+return song;
+}
+
+
+//=============================================
+
+function getHardestHomework(homework){
+
+if (name !== "done"){
+    let sum = 0
+    
+    averageScore 
+}
+
+    return homework;
+}
+return '';
+
+//===============================================
+
+function createPhonebook(names, phonenumbers){
+
+    let phonebook = names + phonebook;
+
+console.log(names[1],":", phonenumbers[1]);
+console.log(names[2],":", phonenumbers[2]);
+console.log(names[3],":", phonenumbers[3]);
+return phonebook
+}

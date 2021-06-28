0f3bfe8dfc489f30223b6cac398797ef91321f1f
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-06-28 10:25:30 -0400

    all

--- main.js
+++ main.js
@@ -4,60 +4,15 @@
 function isAdmin(user){
 
 
-    console.log(userRole)
+    
     if (user.userRole == "ADMIN"){
-        return true("true")
+        return true
    }else if (user.userRole !== "ADMIN"){
-       return false("false")
+       return false
 }
     
 }
 
-//===========================================
-
-function getEmail(user){
-
-  
-}
-
-//===========================================
-
-function getPlaylistLength(name, song){
-
-let song = [];
-
-for (let i =0; i < song.length; i++) {
-    
-}
-return song;
-}
-
-
-//=============================================
-
-function getHardestHomework(homework){
-
-if (name !== "done"){
-    let sum = 0
-    
-    averageScore 
-}
-
-    return homework;
-}
-return '';
-
-//===============================================
-
-function createPhonebook(names, phonenumbers){
-
-    let phonebook = names + phonebook;
-
-console.log(names[1],":", phonenumbers[1]);
-console.log(names[2],":", phonenumbers[2]);
-console.log(names[3],":", phonenumbers[3]);
-return phonebook
-}
 
 
 
